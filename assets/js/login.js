c5166483e824e3164d285eafd3415eefad7152d9
Victor Zion França <vzion435@gmail.com>
2024-10-22 00:34:07 -0300

    Traduzindo

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,13 +1,21 @@
 function logar() {
-
     var login = document.getElementById('login').value;
     var senha = document.getElementById('senha').value;
 
-    if (login == "admin" && senha == "admin") {
-        alert('Sucesso!');
-        location.href = "index.html";
+    // Array contendo objetos de usuários
+    const usuarios = [
+        { usuario: "admin", senha: "admin" },
+        { usuario: "victor", senha: "12345" },
+        { usuario: "marcos", senha: "54321" }
+    ];
+
+    // Verifica se existe um usuário válido
+    const usuarioValido = usuarios.find(u => u.usuario === login && u.senha === senha);
+
+    if (usuarioValido) {
+        location.href = "dashboard.html";
+        alert('Bem vindo!')
     } else {
         alert('Login ou senha incorretos!');
     }
-
 }
