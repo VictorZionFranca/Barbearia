839c8e802fe0a683fbfc88e555f2ed1b315214f0
VictorZionFranca <vzion435@gmail.com>
2024-10-22 21:53:08 -0300

    login

--- assets/js/login.js
+++ assets/js/login.js
@@ -2,23 +2,21 @@ function logar() {
     var login = document.getElementById('login').value;
     var senha = document.getElementById('senha').value;
 
-    // Array contendo objetos de usuários
     const usuarios = [
         { usuario: "admin", senha: "admin" },
         { usuario: "victor", senha: "12345" },
         { usuario: "marcos", senha: "54321" }
     ];
 
-    // Verifica se existe um usuário válido
     const usuarioValido = usuarios.find(u => u.usuario === login && u.senha === senha);
 
     if (usuarioValido) {
-
         document.getElementById('loading').style.display = 'block';
+        localStorage.setItem('usuarioAutenticado', 'true');
 
         setTimeout(() => {
-            location.href = "dashboard.html";
-        }, 650); 
+            location.replace("dashboard.html"); // Usa replace para não manter o histórico
+        }, 650);
     } else {
         alert('Login ou senha incorretos!');
     }
